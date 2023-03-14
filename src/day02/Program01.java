5b0d3b7bde29a975266c5f58ef7f750f3d639162
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-03-14 08:55:15 +0530

    Day03

--- src/day02/Program01.java
+++ src/day02/Program01.java
@@ -3,14 +3,17 @@ package day02;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
 public class Program01 {
 
 	public static void main(String[] args) {
 		 WebDriver driver;	
+		 ChromeOptions options=new ChromeOptions();
+		 options.addArguments("--remote-allow-origins=*");
+		 driver = new ChromeDriver(options);
 		 
-		 driver = new ChromeDriver();
 		 //driver.get("https://demo.automationtesting.in/Register.html");	
 		 driver.navigate().to("https://demo.automationtesting.in/Register.html");
 		 driver.navigate().refresh();
--- src/day02/Program03.java
+++ src/day02/Program03.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Program03 {
 
@@ -18,7 +18,7 @@ public class Program03 {
 	public static void main(String[] args) throws IOException {
 	 	
 	 
-	 driver = new ChromeDriver();
+	 driver = new EdgeDriver();
 	 driver.get("https://demo.automationtesting.in/Register.html");	
 	 driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Test");
 	 screenShot("firstName");
