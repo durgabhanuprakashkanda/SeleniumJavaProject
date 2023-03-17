9f825e1a6b052e6f162d18256eaac0f960812bd9
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-03-17 09:07:19 +0530

    Day05

--- src/day04/Program05.java
+++ src/day04/Program05.java
@@ -1,10 +1,23 @@
 package day04;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Program05 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver;
+		driver = new EdgeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");		
+		driver.findElement(By.xpath("//a[@href='#Textbox']")).click();
+		driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		
+		driver.switchTo().alert().sendKeys("Good Day Tester..!");
+		driver.switchTo().alert().accept();//ok
+		//driver.switchTo().alert().dismiss();//cancel
 	}
 
 }
